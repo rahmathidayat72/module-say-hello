4ff0bdc0200ae225b5a454769165614d82d5fa65
rahmath <hidayat.737rh@gmail.com>
2023-03-28 16:55:12 +0700

    update

--- say-hello.go
+++ say-hello.go
@@ -6,16 +6,16 @@ func SayHello(name string) string {
 
 func Age(age int) string {
 	if age <= 1 {
-		return "Anda di kategorikan Bayi"
+		return "Anda di kategorikan Bayi..!"
 	} else if age <= 5 {
-		return "Anda di kategorikan Balita"
+		return "Anda di kategorikan Balita..!"
 	} else if age <= 12 {
-		return "Anda di kategorikan Anak-anak"
+		return "Anda di kategorikan Anak-anak..!"
 	} else if age <= 18 {
-		return "Anda di kategorikan Remaja"
+		return "Anda di kategorikan Remaja..!"
 	} else if age <= 60 {
-		return "Anda di kategorikan Dewasa"
+		return "Anda di kategorikan Dewasa..!"
 	} else {
-		return "Anda di kategorikan Manula"
+		return "Anda di kategorikan Manula..!"
 	}
 }
